998bfaa4df96eeb92ae68a9e6ea3d4e9ec144b73
mgorsk1 <gorskimariusz13@gmail.com>
2020-04-21 09:23:57 +0200

    :bug: fix spinner

--- garbage_detector/utils/spinner.py
+++ garbage_detector/utils/spinner.py
@@ -6,11 +6,39 @@ from gpiozero import LEDBoard
 from garbage_detector import config
 
 
-class LEDBoardSingleton:
-    board = LEDBoard(*[int(v) for k, v in config.leds.items()])
+class SpinnerThread(Thread):
+    def __init__(self,  *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.runs = 0
+        self.blink_time = 0.5
+        self.blink_on_finish = False
+        self.board = LEDBoard(*[int(v) for k, v in config.leds.items()])
+
+        self.killed = False
+
+    def stop(self):
+        self.killed = True
+
+    def run(self):
+        while not self.killed or self.runs < 2:
+            for i in range(len(self.board)):
+                self.board.toggle(i)
+                sleep(0.2)
+                self.board.toggle(i)
+
+            self.runs += 1
+
+        sleep(self.blink_time)
+
+        if self.blink_on_finish:
+            self.board.blink(on_time=self.blink_time, off_time=self.blink_time, n=3, background=False)
+
+        self.board.off()
+
+        raise SystemExit()
 
 
-class LEDBoardSpinner(LEDBoardSingleton):
+class LEDBoardSpinner:
     blink_on_finish = False
 
     def __init__(self, function, **kwargs):
@@ -22,34 +50,20 @@ class LEDBoardSpinner(LEDBoardSingleton):
 
     def __call__(self, *args, **kwargs):
         if self.function is not None:
-            thread = Thread(target=self.run, args=())
+            spinner = SpinnerThread(args=(self.blink_on_finish))
 
-            thread.start()
+            spinner.blink_on_finish = self.blink_on_finish
+
+            spinner.start()
 
             result = self.function(*args, **kwargs)
 
-            while thread.is_alive():
+            spinner.stop()
+
+            while spinner.is_alive():
                 pass
 
-            if self.blink_on_finish:
-                self.board.blink(on_time=self.blink_time, off_time=self.blink_time, n=3, background=False)
-
-            self.board.off()
-
             return result
-
-    def run(self):
-        runs = 0
-
-        while True and runs < 2:
-            for i in range(len(self.board)):
-                self.board.toggle(i)
-                sleep(0.2)
-                self.board.toggle(i)
-
-            runs += 1
-
-        sleep(self.blink_time)
 
 
 class LEDBoardSpinnerWithConfirmation(LEDBoardSpinner):
