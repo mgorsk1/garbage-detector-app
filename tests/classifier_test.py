0e7c39e59007791557d7e86fab678a96afc65db1
Niels Denissen <nielsdenissen@gmail.com>
2020-04-22 09:52:53 +0200

    Fix renames

--- tests/classifier_test.py
+++ tests/classifier_test.py
@@ -1,11 +1,12 @@
-import os
-
 from garbage_detector.classifier import GarbageClassifier
+import os
+import cv2
 
 
 # Determine current dir
 current_dir = os.path.dirname(__file__)
 
 if __name__ == '__main__':
+    image = cv2.imread(f'{current_dir}/resources/plastic.jpg')
     classifier = GarbageClassifier()
-    classifier.classify(f'{current_dir}/resources/plastic.jpg')
+    classifier.classify(image=image)
